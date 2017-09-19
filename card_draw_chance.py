1c42e3aba828a95159af824f54a7619b00ad1d1f
IronHeart7334 <greengrappler12@gmail.com>
2017-09-19 08:59:04 -0700

    Should work now
    Thanks to Ian from Tales From the Cards

--- card_draw_chance.py
+++ card_draw_chance.py
@@ -1,12 +1,33 @@
+def int_input(msg):
+  inp = raw_input(msg)
+  try:
+    inp = int(inp)
+    if inp <= 0:
+      print("Please enter a non-negative number")
+      int_input(msg)
+    else:
+      return inp
+  
+  except:
+    print("Please enter a valid non-decimal number")
+    int_input(msg)
+
 def run():
   global starting_hand
   global num_copies
   global deck_size
   global draw_chance
-  starting_hand = int(raw_input("Starting hand size: "))
-  num_copies = float(raw_input("Number of copies: "))
-  deck_size = float(raw_input("Deck size: "))
+  global not_draw
+  
+  starting_hand = int_input("Starting hand size: ")
+  num_copies = int_input("Number of copies: ")
+  
+  deck_size = 0
+  while num_copies > deck_size:
+    deck_size = int_input("Deck size: ")
+  
   draw_chance = 0.0
+  not_draw = 1.0;
   test()
   cont = raw_input("Do you wish to continue? y/n")
   if cont == "y":
@@ -14,10 +35,15 @@ def run():
 
 def draw():
   global draw_chance
+  global not_draw
   global deck_size
-  draw_chance += num_copies / deck_size
+  
+  not_draw *= float((deck_size - num_copies)) / deck_size
+  draw_chance = 1.0 - not_draw
+  
   if draw_chance >= 1.0:
   	draw_chance = 1.0
+  
   deck_size -= 1
   
 def draw_start():
