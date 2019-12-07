32b85da9d2eb6ec412a38f24e9f1d7120930f5c2
IronHeart7334 <greengrappler12@gmail.com>
2019-12-07 10:44:25 -0800

    started work on diceChance

---
+++ diceChance.py
@@ -0,0 +1,26 @@
+"""
+Calculates the chance of rolling specific numbers or higher
+using a set of dice (d4, d6, d20, etc)
+"""
+
+"""
+Caclulates the probability
+that the sum of all dice rolled
+will be at least x
+"""
+def chanceXPlus(dice, x):
+    chance = 0
+    if not isinstance(dice, list):
+        dice = [dice]
+    for die in dice:
+        print(die)
+    return chance
+
+def testAll():
+    dice = [4, 6, 8, 10, 20] #might be up to 2 more
+    for die in dice:
+        for i in range(1, die + 1):
+            print("Chance to roll " + str(i) + " or more using a d" + str(die) + ": " + str(chanceXPlus(die, i)))
+    chanceXPlus(dice, 20)
+
+testAll()
