eeb4b118dbf1f50a5a51be331b765b484c5c58c4
IronHeart7334 <greengrappler12@gmail.com>
2019-12-13 10:40:26 -0800

    looks like vertex stuff is done

---
+++ graphs/graphs.py
@@ -0,0 +1,57 @@
+class Graph:
+    def __init__(self):
+        self.vertices = []
+        self.edges = dict()
+    def hasVertex(self, v):
+        #binary search
+        min = 0
+        max = len(self.vertices)
+        found = False
+        while not found and min < max and min >= 0 and max < len(self.vertices):
+            mid = int((min + max) / 2)
+            print(mid)
+            if self.vertices[mid] > v:
+                max = mid
+            elif self.vertices[mid] < v:
+                min = mid
+            else:
+                found = True
+        return found
+    def hasEdge(self, start, end):
+        return start in self.edges and end in self.edges[start]
+
+    def addVertex(self, v):
+        if not self.hasVertex(v):
+            self.vertices.append(v)
+            #insertion sort
+            i = len(self.vertices) - 1
+            while i > 0 and self.vertices[i] < self.vertices[i - 1]:
+                temp = self.vertices[i]
+                self.vertices[i] = self.vertices[i - 1]
+                self.vertices[i - 1] = temp
+                i -= 1
+
+    def addEdge(self, start, end, weight):
+        if not start in self.edges:
+            self.edges[start] = dict()
+        if not end in self.edges[start]:
+            self.edges[start][end] = weight
+
+    def print(self):
+        print("VERTICES:")
+        print(self.vertices)
+        print("EDGES:")
+        for start in self.edges.keys():
+            for end in self.edges[start].keys():
+                print(str(start) + ", " + str(end) + ": " + str(self.edges[start][end]))
+
+def graphFromFile(vertexFile, edgeFile):
+    g = Graph()
+    vFile = open(vertexFile)
+    for line in vFile:
+        g.addVertex(int(line))
+        g.print()
+
+
+if __name__ == "__main__":
+    graphFromFile("vertices.txt", "")
