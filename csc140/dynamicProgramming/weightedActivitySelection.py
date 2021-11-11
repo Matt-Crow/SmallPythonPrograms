7c075e3d2a09c0894d9e42e6a3f67b092a19983c
Matt Crow <greengrappler12@gmail.com>
2021-11-11 14:23:40 -0800

    Going well so far, only taken me over an hour, and I just need the dynamic solution

--- csc140/dynamicProgramming/weightedActivitySelection.py
+++ csc140/dynamicProgramming/weightedActivitySelection.py
@@ -10,7 +10,22 @@ Need to solve by finding the best activity to fill each block of time, build up
 to larger blocks
 
 Recursive solution:
-    
+    T(A, end): # professor goes backward
+        if end < 0:
+            return []
+
+        choice = A[end]
+        i = end - 1
+        while i >= 0 and A[i] is not compatible with choice:
+            i -= 1
+        # by now, i is either -1 or points to the next compatible job
+        endIsChosen = T(A, i) union choice
+        endNotChosen = T(A, end - 1)
+        if total(endIsChosen) < total(endNotChosen):
+            return endNotChosen
+        else:
+            return endIsChosen
+
 """
 
 class Activity:
@@ -24,11 +39,83 @@ class Activity:
 
 def test():
     activites = [
-
+        Activity(0, 1, 3), # 3
+        Activity(1, 3, 2), #  22
+        Activity(0, 2, 2), # 22
+        Activity(2, 3, 4)  #   4
     ]
+    sol = maxWeight(activites) # should be (0, 1, 3) and (2, 3, 4)
+    print("Best:")
+    printActs(sol)
+
+def maxWeight(activites):
+    s = sorted(activites, key=lambda act : act.end)
+    printActs(s)
+    return memoized(s, len(s) - 1)
+
+# works
+def recurImpl(acts, lastIdx):
+    if lastIdx < 0:
+        return []
+
+    choice = acts[lastIdx]
+    compIdx = lastIdx - 1
+    while compIdx >= 0 and not areCompatible(choice, acts[compIdx]):
+        compIdx -= 1
+
+    # ... what if we choose the last?
+    lastChosen = recurImpl(acts, compIdx)
+    lastChosen.append(choice)
+    # ... and what if we don't?
+    lastNotChosen = recurImpl(acts, lastIdx - 1)
+
+    if total(lastChosen) < total(lastNotChosen):
+        return lastNotChosen
+    return lastChosen
+
+# works
+def memoized(acts, lastIdx, cache=None):
+    if lastIdx < 0:
+        return []
+
+    if cache is None:
+        cache = [None for a in acts]
+    # cache[i] holds the best choices for acts[0:i]
+    if cache[lastIdx] is not None:
+        print("cached")
+        return cache[lastIdx]
+
+    choice = acts[lastIdx]
+    compIdx = lastIdx - 1
+    while compIdx >= 0 and not areCompatible(choice, acts[compIdx]):
+        compIdx -= 1
+
+    # ... what if we choose the last?
+    lastChosen = memoized(acts, compIdx, cache)
+    lastChosen.append(choice)
+    # ... and what if we don't?
+    lastNotChosen = memoized(acts, lastIdx - 1)
+
+    if total(lastChosen) < total(lastNotChosen):
+        return lastNotChosen
+    else:
+        cache[lastIdx] = lastChosen
+    return cache[lastIdx]
 
 def areCompatible(a1, a2):
     a1BeforeA2 = a1.end <= a2.start
     a2BeforeA1 = a2.end <= a1.start
 
     return a1BeforeA2 or a2BeforeA1
+
+def total(acts):
+    sum = 0
+    for a in acts:
+        sum += a.weight
+    return sum
+
+def printActs(acts):
+    print(", ".join(str(a) for a in acts))
+
+if __name__ == "__main__":
+    test()
